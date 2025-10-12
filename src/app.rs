cfa6356d8acfb9fb99a1fe3c5aed4e610dc9bbd7
internet_wizard <umex28@gmail.com>
2025-10-12 02:48:59 -0500

    Initial foray into SSG with multiple pages

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,9 @@ use leptos::prelude::*;
 
 #[component]
 pub fn App() -> impl IntoView {
+    // Add a 404
+    let fallback = || view! { "Page not found" }.into_view();
+
     view! {
         <components::Intro />
         <components::StatusCafe />
